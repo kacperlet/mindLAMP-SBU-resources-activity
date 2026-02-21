0643cfeb72b8ef55f29f737296bacb42726befd1
Kacper Letowski <kacperlet27@gmail.com>
2026-02-21 18:07:05 -0500

    initialized basic react app

---
+++ src/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+function Home() {
+    return <h1>Home Page</h1>;
+}
+
+function App() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+            </Routes>
+        </BrowserRouter>
+    );
+}
+
+export default App;
