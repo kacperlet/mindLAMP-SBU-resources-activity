a02363938f86f3d65e46ee56c3f02b9e67881d77
Kacper Letowski <kacperlet27@gmail.com>
2026-03-27 00:55:14 -0400

    continued implementation of quiz component

--- src/components/QuizPage.js
+++ src/components/QuizPage.js
@@ -10,14 +10,35 @@ function Quiz() {
 
     const questions = [
         {
+            question: "If you arrive at a four-way stop at the same time as another vehicle, what is the safest next step?",
+            choices: ["Proceed quickly to avoid delaying traffic", "Yield and allow the other vehicle to go first", "Wait for the other driver to signal or proceed", "Honk to indicate your turn"],
+            correctChoice: 1, // B
+            explanation: "At a four-way stop, if two vehicles arrive at the same time, the safest choice is to yield and let the other vehicle go first (especially if there's any uncertainty). This helps prevent confusion and reduces the risk of a collision. Proceeding quickly or honking can startle other drivers and increase danger, and waiting for a signal can lead to hesitation or miscommunication. Yielding is predictable, calm, and keeps everyone safe."
+        },
+        {
+            question: "If a driver is following you too closely, what are appropriate actions you can take?",
+            choices: ["Speed up to increase distance", "Maintain a steady speed and allow space ahead", "Change lanes when safe to do so", "Pull over when it is safe and let the vehicle pass"],
+            correctChoice: 2,
+            explanation: "When someone is following too closely, the main hazard is that they don't have enough time to react if you need to slow down or stop. By safely changing lanes, you remove yourself from that immediate risk zone and allow the aggressive or inattentive driver to pass. This is a defensive driving strategy-it prioritizes avoiding conflict rather than trying to control the other driver's behavior."
+        },
+        {
             question: "This is a sample question?",
-            choices: ["Sample Choice A", "Sample Choice B", "Sample Choice D", "Sample Choice D"],
-            correctChoice: 2
+            choices: ["Sample Choice A", "Sample Choice B", "Sample Choice C", "Sample Choice D"],
+            correctChoice: 2,
+            explanation: "This is a sample explanation. This will explain why it is the correct answer choice."
         },
 
     ]
 
+    const incrementQuestion = () => {
+        if (questionIndex + 1 < questions.length)
+            setQuestionIndex(questionIndex+1);
+        else
+            setQuestionIndex(0)
+    }
+
     const currentQuestion = questions[questionIndex];
+    const progress = (100 * (questionIndex+1.0)/questions.length) + '%'
     
     return (
         <div id="quiz">
@@ -26,12 +47,16 @@ function Quiz() {
                 <span>Practice</span>
                 <IconDots  />
             </div>
+            <div id="progress-bar" style={{width: progress}}></div>
             <div id="quiz-body">
                 <span>Question {questionIndex+1} out of {questions.length}</span>
                 <QuizQuestion 
+                    key={questionIndex}
                     question={currentQuestion.question}
                     choices={currentQuestion.choices}
                     correctChoice={currentQuestion.correctChoice}
+                    explanation={currentQuestion.explanation}
+                    incrementQuestion={incrementQuestion}
                 />
             </div>                    
         </div>
--- src/components/QuizQuestion.js
+++ src/components/QuizQuestion.js
@@ -1,24 +1,35 @@
 import { useState } from "react";
 
 function QuizQuestion(props) {
-    const { question, choices, correctChoice, test } = props;
+    const { question, choices, correctChoice, explanation, incrementQuestion } = props;
 
     const [ selected, setSelected ] = useState(-1);
     const [ isCorrect, setIsCorrect ] = useState(null);
+    const [ showExplanation, setShowExplanation ] = useState(false);
 
     const handleChange = (e) => {
         setSelected(e.target.value)
     }
 
     const handleClick = (e) => {
-        if (selected == correctChoice)
+        // Check if answer is correct
+        if (isCorrect === null)
         {
-            setIsCorrect(true);
+            if (selected == correctChoice)
+            {
+                setIsCorrect(true);
+            }
+            else
+            {
+                setIsCorrect(false);
+            }
         }
+        // Show explanation
         else
         {
-            setIsCorrect(false);
+            setShowExplanation(true);
         }
+        
     }
 
     let message = ""
@@ -30,33 +41,54 @@ function QuizQuestion(props) {
     }
     else if (isCorrect === false) // if null, message shouldn't be visible
     {
-        message = "That's wrong :( \nTry again?"
+        message = "That's wrong :("
         messageClass += " wrong"
     }
 
-    return (
-        <div className='quiz-question'>
-            <span>{question}</span>
-            <form className="quiz-choices">
-                {
-                choices.map((choice, index) => {
-                    return (
-                        <div key={index}>
-                            <input type="radio" id={index} name="question" value={index} onChange={handleChange}></input>
-                            <label htmlFor={index}>{choice}</label>
-                        </div>
-                    )
-                })
-            }
-            </form>
-            <span className={messageClass}>{message}</span>
-            <button onClick={handleClick} disabled={selected===-1}>
-                Check
-            </button>
-            
-            
-        </div>
-    )
+    const disabled = isCorrect !== null;
+    let buttonText = "Check";
+    if (disabled)
+        buttonText = "Show Explanation"
+
+
+    if (!showExplanation)
+        return (
+            <div className='quiz-question'>
+                <span>{question}</span>
+                <form className="quiz-choices">
+                    {
+                    choices.map((choice, index) => {
+                        return (
+                            <div key={index}>
+                                <input disabled={disabled} type="radio" id={index} name="question" value={index} onChange={handleChange}></input>
+                                <label htmlFor={index}>{choice}</label>
+                            </div>
+                        )
+                    })
+                }
+                </form>
+                <span className={messageClass}>{message}</span>
+                <button onClick={handleClick} disabled={selected===-1}>
+                    {buttonText}
+                </button>
+                
+                
+            </div>
+        )
+    else
+        return (
+            <div className='quiz-question'>
+                <span>{question}</span>
+                <div className="quiz-question-explanation">
+                    {explanation}
+                </div>
+                <button onClick={incrementQuestion}>
+                    Next Question
+                </button>
+                
+                
+            </div>
+        )
 }
 
 export default QuizQuestion
